f767c2110923c7b9eb0150855c7f2adbb74e3207
patrikcepr <wkidmusic@gmail.com>
2021-06-08 01:23:23 +0200

    web version 1.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,22 @@
-console.log('Higher baby');
+console.log("Higher baby");
 
-// < script type = "text/javascript" > 
+//menu hide and show
 document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 200) {
-            document.getElementById("navbar_top").classList.add("fixed-top");
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector(".navbar").offsetHeight;
-            document.body.style.paddingTop = navbar_height + "px";
-        } else {
-            document.getElementById("navbar_top").classList.remove("fixed-top");
-            // remove padding top from body
-            document.body.style.paddingTop = "0";
-        }
-    });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
+  });
 });
-// DOMContentLoaded  end
-// </script>
+
+// collapse menu on click
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
